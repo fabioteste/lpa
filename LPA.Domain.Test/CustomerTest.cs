f80df8f48f10edaa48b38174a924429d3b3ded39
fabio <fabio.emailteste@gmail.com>
2017-06-25 17:31:49 -0300

    teste de dominio

---
+++ LPA.Domain.Test/CustomerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using LPA.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LPA.Domain.Test
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        private readonly User user = new User("fabiorezende", "senha");
+
+        [TestMethod]
+        [TestCategory("Customer - New Customer")]
+        public void dado_um_nome_invalido_deve_retornar_uma_notificacao()
+        {
+            var customer = new Customer("f", "rezende", "fabio.emailteste@gmail.com", user);
+            Assert.IsFalse(customer.IsValid());
+        }
+
+        [TestMethod]
+        [TestCategory("Customer - New Customer")]
+        public void dado_um_sobrenome_invalido_deve_retornar_uma_notificacao()
+        {
+            var customer = new Customer("fabio", "r", "fabio.emailteste@gmail.com", user);
+            Assert.IsFalse(customer.IsValid());
+        }
+
+        [TestMethod]
+        [TestCategory("Customer - New Customer")]
+        public void dado_um_email_invalido_deve_retornar_uma_notificacao()
+        {
+            var customer = new Customer("fabio", "rezende", "fabio", user);
+            Assert.IsFalse(customer.IsValid());
+        }
+    }
+}
